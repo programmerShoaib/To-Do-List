27aa28357e3aa32b9aa01b36761afe88c02004cb
programmerShoaib <link2shoaib.2002@gmail.com>
2025-01-28 16:37:15 +0500

    add new function to add new taskobject

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const list = document.querySelector('ul');
-const input = document.querySelector('input');
+const tasks = new Array();
 
-function addItem() {
-    const item = document.createElement('li');
-    item.textContent = input.value;
-    list.appendChild(item);
-    input.value = '';
-}
+function addTask() {
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+    const task = document.getElementById("task").value;
+
+    const taskObject = {
+        title: title,
+        description: description,
+        task: task
+    };
+    tasks.push(taskObject);
+
+    const taskList = document.getElementById("task-list");
+    const taskItem = document.createElement("li");
+    taskItem.innerHTML = `
+        <h3>${title}</h3>
+        <p>${description}</p>
+        <p>${task}</p>
+    `;
+    taskList.appendChild(taskItem);
+    }
